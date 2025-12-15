c60b56558528fbb870826bc0d1327f0caaa975f5
Yashosw10 <yashosw10@gmail.com>
2025-12-15 12:38:26 +0530

    register work done

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -36,11 +36,16 @@ const registerUser = asyncHandler(async (req, res)=>{
     if (existedUser) {
         throw new ApiError(409,"User with email or username already exist")
     }
-    console.log(req.files);
+    //console.log(req.files);
 
     //Handling Images
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
